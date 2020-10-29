d6cc83567f2127d18dfeeeedaa52bf85be0731be
Tomas Ralbovsky <tomralbovsky@gmail.com>
2020-10-29 21:26:44 +0100

    Hotove zadanie
    Pridanie celeho hotoveho zadania c.6

---
+++ cv_6_v1/Core/Src/main.c
@@ -0,0 +1,173 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2020 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "usart.h"
+#include "gpio.h"
+
+void SystemClock_Config(void);
+
+void process_serial_data(uint8_t ch);
+
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+
+  LL_APB2_GRP1_EnableClock(LL_APB2_GRP1_PERIPH_SYSCFG);
+  LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_PWR);
+
+  NVIC_SetPriorityGrouping(NVIC_PRIORITYGROUP_4);
+
+  /* System interrupt init*/
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_USART2_UART_Init();
+
+  USART2_RegisterCallback(process_serial_data);
+
+  while (1)
+  {
+	  LL_mDelay(1000);
+
+	  	  if((LL_GPIO_ReadInputPort(GPIOB) & (1 << 3)) >> 3)
+	  	  {
+	  		  LL_USART_TransmitData8(USART2, '1');
+	  	  }
+	  	  else
+	  	  {
+	  		  LL_USART_TransmitData8(USART2, '0');
+	  	  }
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  LL_FLASH_SetLatency(LL_FLASH_LATENCY_0);
+  while(LL_FLASH_GetLatency()!= LL_FLASH_LATENCY_0)
+  {
+  }
+  LL_RCC_HSI_Enable();
+
+   /* Wait till HSI is ready */
+  while(LL_RCC_HSI_IsReady() != 1)
+  {
+
+  }
+  LL_RCC_HSI_SetCalibTrimming(16);
+  LL_RCC_SetAHBPrescaler(LL_RCC_SYSCLK_DIV_1);
+  LL_RCC_SetAPB1Prescaler(LL_RCC_APB1_DIV_1);
+  LL_RCC_SetAPB2Prescaler(LL_RCC_APB1_DIV_1);
+  LL_RCC_SetSysClkSource(LL_RCC_SYS_CLKSOURCE_HSI);
+
+   /* Wait till System clock is ready */
+  while(LL_RCC_GetSysClkSource() != LL_RCC_SYS_CLKSOURCE_STATUS_HSI)
+  {
+
+  }
+  LL_Init1msTick(8000000);
+  LL_SYSTICK_SetClkSource(LL_SYSTICK_CLKSOURCE_HCLK);
+  LL_SetSystemCoreClock(8000000);
+}
+
+void process_serial_data(uint8_t ch)
+{
+	static uint8_t count_ON = 0, count_OFF=0;
+	static char vzor_ON[]="ledON", vzor_OFF[]="ledOFF";
+
+	if(vzor_ON[count_ON]==ch){
+		count_ON++;
+		if(count_ON==5){
+			LL_GPIO_SetOutputPin(GPIOB, LL_GPIO_PIN_3);
+			count_ON=0;
+		}
+	}
+	else{
+		count_ON=0;
+		if(vzor_ON[count_ON]==ch){
+			count_ON++;
+		}
+	}
+
+	if(vzor_OFF[count_OFF]==ch){
+		count_OFF++;
+		if(count_OFF==6){
+			LL_GPIO_ResetOutputPin(GPIOB, LL_GPIO_PIN_3);
+			count_OFF=0;
+		}
+	}
+	else{
+		count_OFF=0;
+		if(vzor_OFF[count_OFF]==ch){
+			count_OFF++;
+		}
+	}
+}
+
+
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     tex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
